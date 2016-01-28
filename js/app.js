e66e409fdbbfe7ef24e7447783087c6a33fc7c8f
Calum-Campbell <campbell.cj@hotmail.co.uk>
2016-01-28 17:34:12 +0000

    js, stop default opening of files outside of dropzone

--- js/app.js
+++ js/app.js
@@ -7,23 +7,37 @@ var DragAndDrop = DragAndDrop || {}
 DragAndDrop.go = function(){
   var dropzone = $('#dropzone')
 
-  dropzone.on('dragenter', function(a){
-    a.stopPropagation();
-    a.preventDefault();
+  dropzone.on('dragenter', function(enter){
+    enter.stopPropagation();
+    enter.preventDefault();
     console.log("dragged in")
   });
 
-  dropzone.on('dragover', function(b){
-    a.stopPropagation();
-    a.preventDefault();
+  dropzone.on('dragover', function(over){
+    over.stopPropagation();
+    over.preventDefault();
   })
 
-  dropzone.on('drop', function(c){
+  dropzone.on('drop', function(drop){
 
-    c.preventDefault();
+    drop.preventDefault();
   })
 
+  //Prevent file opening in browser
 
+  $(document).on('dragenter', function (enter) 
+  {
+    enter.stopPropagation();
+    enter.preventDefault();
+  });
+  $(document).on('dragover', function (over) 
+  {
+    over.stopPropagation();
+    over.preventDefault();
+  });
+  $(document).on('drop', function (drop) 
+  {
+    drop.stopPropagation();
+    drop.preventDefault();
+  });
 
-
-}
