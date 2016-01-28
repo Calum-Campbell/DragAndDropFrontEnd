10a2e9ba92330b37e62485edfd859dc872cef5fa
Calum-Campbell <campbell.cj@hotmail.co.uk>
2016-01-28 17:23:16 +0000

    drag and drop .on function implemented

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DragAndDropFrontEnd
 Front End Drag and Drop Page
+
+
+## Process
+
+### Set up Boilerplate
+1. wdi-london-alumni
+2. Set up basic frontend App, Html,css,js
+3. Use Bower to manage dependensies
+
+### Get some basic functionality
+1. Set up basic html and css so can see whats happening in browser
+
+```
+#dropzone {
+  width: 200px;
+  height:200px;
+  border: 2px dashed red;
+  color: blue;
+  line-height: 300px;
+  text-align: center;
+}
+```
--- js/app.js
+++ js/app.js
@@ -5,5 +5,25 @@ $(function(){
 var DragAndDrop = DragAndDrop || {} 
 
 DragAndDrop.go = function(){
-  console.log("hello")
+  var dropzone = $('#dropzone')
+
+  dropzone.on('dragenter', function(a){
+    a.stopPropagation();
+    a.preventDefault();
+    console.log("dragged in")
+  });
+
+  dropzone.on('dragover', function(b){
+    a.stopPropagation();
+    a.preventDefault();
+  })
+
+  dropzone.on('drop', function(c){
+
+    c.preventDefault();
+  })
+
+
+
+
 }
